b0bf30adfad57f05361a1df16c0c329e58d7c5b4
Danika01 <168979440+Danika01@users.noreply.github.com>
2025-10-03 18:24:08 +0800

    feat: implement romanToDecimal for numbers to pass test

--- src/moduleA.js
+++ src/moduleA.js
@@ -2,8 +2,43 @@ const functions = {
 
 //to be implemented by dev b
 
-//   romanToDecimal: (roman) => {
-//   }
+   romanToDecimal: (roman) => {
+    if (typeof roman !== 'string')
+    {
+        throw new Error('Invalid input: Input should be a string.')
+    }
+
+    const romanNum ={
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    // dev A create a block of code to handle numbers
+
+
+    let total = 0;
+    for(let i = 0; i < roman.length; i++)
+    {
+      const current = romanNum[roman[i]];
+      const next = romanNum[roman[i + 1]];
+
+      if(next && current < next)
+      {
+        total -= current;
+      }
+      else 
+      {
+        total += current;
+      }
+    }
+    return total;
+
+   }
 
 
 };
